e8a6d559f0ecfe80de32ff8c75865b1754d01751
zajko04 <Grepolis98IM!>
2019-07-12 17:45:31 +0200

    elo

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.recycleview;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerViewAccessibilityDelegate;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
+    private LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         recyclerView = findViewById(R.id.recycle);
         recyclerView.setHasFixedSize(true);
@@ -25,18 +41,56 @@ public class MainActivity extends AppCompatActivity {
 
         List<Item> items = new ArrayList<>();
         for(int i=0; i<50;i++){
-            items.add(new Item("Numer "+i+1,false));
+            items.add(new Item("   Numer "+String.valueOf(i+1),false));
         }
 
         RecyclerAdapter recyclerAdapter = new RecyclerAdapter(items,this);
 
+
         recyclerView.setAdapter(recyclerAdapter);
 
+        ItemDecoration itemDecoration = new ItemDecoration(10);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+}
+
+class ItemDecoration extends RecyclerView.ItemDecoration{
+
+    private int space;
+
+    public ItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+
+        int maxBottom = 0;
+        final Rect childRect = new Rect();
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            View child = parent.getChildAt(i);
+            parent.getDecoratedBoundsWithMargins(child, childRect);
+
+            child.semSetRoundedCorners(15);
+
+        }
 
 
 
     }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.left = space;
+        outRect.right = space;
+        outRect.bottom = space;
+
+        // Add top margin only for the first item to avoid double space between items
+        if(parent.getChildAdapterPosition(view) == 0) {
+            outRect.top = space;
+        }
+    }
 }
 
-class RecycleViewAdapter{
-}
+
